ff1f3c20e07d71f390d61fe628f4fdafa1c5b98a
Víctor San Martín <vsanmartinm@gmail.com>
2014-07-01 22:28:00 -0400

    Test

--- Test/Case/Lib/EnvironmentTest.php
+++ Test/Case/Lib/EnvironmentTest.php
@@ -40,4 +40,54 @@ class EnvironmentTest extends CakeTestCase {
 		Environment::set('production');
 		$this->assertTrue(Environment::is('production'));
 	}
+
+	public function testSetAndGet() {
+		Environment::set('development');
+		$this->assertEquals(Environment::get(), 'development');
+
+		Environment::set('other');
+		$this->assertEquals(Environment::get(), 'other');
+
+		$this->assertTrue(Environment::set() == false);
+	}
+
+	public function testWriteAndRead() {
+		Environment::write('title', 'this is development', 'development');
+		$this->assertEquals(Environment::read('title', 'development'), 'this is development');
+
+		Environment::write('title', 'this is production', 'production');
+		$this->assertEquals(Environment::read('title', 'production'), 'this is production');
+
+		Environment::set('development');
+		$this->assertEquals(Environment::read('title'), 'this is development');
+
+		Environment::set('production');
+		$this->assertEquals(Environment::read('title'), 'this is production');
+
+		Environment::write(array(
+			'hello' => 'Hello World',
+			'bye' => 'Bye bye'
+		), 'development');
+		$this->assertEquals(Environment::read('bye', 'development'), 'Bye bye');
+
+		Environment::write(array(
+			'debug' => 3,
+			'App.encoding' => 'UTF-8',
+			'Security.salt' => 'hjkh34k5j3kKjg23kH23452873fd9sdfsd9f8shd90f'
+		), 'development');
+
+		Environment::write(array(
+			'debug' => 0,
+			'App.encoding' => 'UTF-8',
+			'Security.salt' => '67783jd9HKkjkhhkferwQhhPbkjb987987bJKhjvdskhJK'
+		), 'production');
+
+		Environment::set('development');
+		$this->assertEquals(Configure::read('Security.salt'), 'hjkh34k5j3kKjg23kH23452873fd9sdfsd9f8shd90f');
+
+		Environment::set('production');
+		$this->assertEquals(Configure::read('Security.salt'), '67783jd9HKkjkhhkferwQhhPbkjb987987bJKhjvdskhJK');
+
+	}
+
 }
